a616442d45382a5d39c17acbaeb29f0819307aab
Bhavishradhan E P <81689045+bhavishradhan@users.noreply.github.com>
2023-03-31 15:21:47 +0530

    Create binary search.cpp

---
+++ binary search.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
